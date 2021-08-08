d1f94c5e029357167b871375b1a743df59d330bc
liuqiang <liuqiang.06@bytedance.com>
2021-08-08 23:54:25 +0800

    feat: optimize quicksort tail call

--- encoder/kvsort.go
+++ encoder/kvsort.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 ByteDance Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
 package encoder
 
 import (
@@ -7,43 +23,69 @@ import (
 // Algorithm 3-way Radix Quicksort, d means the radix.
 // Reference: https://algs4.cs.princeton.edu/51radix/Quick3string.java.html
 func radixQsort(kvs kvSlice, d, maxDepth int) {
-    if len(kvs) < 12 {
-        insertRadixSort(kvs, 0, len(kvs), d)
-        return
-    }
     if maxDepth == 0 {
         heapSort(kvs, 0, len(kvs))
         return
     }
     maxDepth--
+    for len(kvs) > 11 {
+        p := pivot(kvs, d)
+        lt, i, gt := 0, 0, len(kvs)
+        for i < gt {
+            c := byteAt(kvs[i].k, d)
+            if c < p {
+                swap(kvs, lt, i)
+                i++
+                lt++
+            } else if c > p {
+                gt--
+                swap(kvs, i, gt)
+            } else {
+                i++
+            }
+        }
 
-    p := pivot(kvs, d)
-    lt, i, gt := 0, 0, len(kvs)
-    for i < gt {
-        c := byteAt(kvs[i].k, d)
-        if c < p {
-            swap(kvs, lt, i)
-            i++
-            lt++
-        } else if c > p {
-            gt--
-            swap(kvs, i, gt)
+        // kvs[0:lt] < v = kvs[lt:gt] < kvs[gt:len(kvs)]
+        // Native:
+        // radixQsort(kvs[:lt], d, maxDepth)
+        // if p > -1 {
+        //     radixQsort(kvs[lt:gt], d+1, maxDepth)
+        // }
+        // radixQsort(kvs[gt:], d, maxDepth)
+        // Optimize: make recursive calls only for the smaller parts.
+        // Reference: https://www.geeksforgeeks.org/quicksort-tail-call-optimization-reducing-worst-case-space-log-n/
+        if p == -1 {
+            if lt > len(kvs) - gt {
+                radixQsort(kvs[gt:], d, maxDepth)
+                kvs = kvs[:lt]
+            } else {
+                radixQsort(kvs[:lt], d, maxDepth)
+                kvs = kvs[gt:]
+            }
         } else {
-            i++
+            ml := maxThree(lt, gt-lt, len(kvs)-gt)
+            if ml == lt {
+                radixQsort(kvs[lt:gt], d+1, maxDepth)
+                radixQsort(kvs[gt:], d, maxDepth)
+                kvs = kvs[:lt]
+            } else if ml == gt-lt {
+                radixQsort(kvs[:lt], d, maxDepth)
+                radixQsort(kvs[gt:], d, maxDepth)
+                kvs = kvs[lt:gt]
+                d += 1
+            } else {
+                radixQsort(kvs[:lt], d, maxDepth)
+                radixQsort(kvs[lt:gt], d+1, maxDepth)
+                kvs = kvs[gt:] 
+            }
         }
     }
-
-    // kvs[0:lt] < v = kvs[lt:gt] < kvs[gt:len(kvs)]
-    radixQsort(kvs[:lt], d, maxDepth)
-    if p > -1 {
-        radixQsort(kvs[lt:gt], d+1, maxDepth)
-    }
-    radixQsort(kvs[gt:], d, maxDepth)
+    insertRadixSort(kvs, d)
 }
 
-func insertRadixSort(kvs kvSlice, lo, hi, d int) {
-    for i := lo + 1; i < hi; i++ {
-        for j := i; j > lo && lessFrom(kvs[j].k, kvs[j-1].k, d); j-- {
+func insertRadixSort(kvs kvSlice, d int) {
+    for i := 1; i < len(kvs); i++ {
+        for j := i; j > 0 && lessFrom(kvs[j].k, kvs[j-1].k, d); j-- {
             kvs[j], kvs[j-1] = kvs[j-1], kvs[j]
         }
     }
@@ -52,7 +94,7 @@ func insertRadixSort(kvs kvSlice, lo, hi, d int) {
 func pivot(kvs kvSlice, d int) int {
     m := len(kvs) >> 1
     if len(kvs) > 40 {
-        // Tukey'kvs ``Ninther,'' median of three mediankvs of three.
+        // Tukey's ``Ninther,'' median of three mediankvs of three.
         t := len(kvs) / 8
         return medianThree(
             medianThree(byteAt(kvs[0].k, d), byteAt(kvs[t].k, d), byteAt(kvs[2*t].k, d)),
@@ -67,7 +109,7 @@ func pivot(kvs kvSlice, d int) int {
 func medianThree(i, j, k int) int {
     if i > j {
         i, j = j, i
-    }
+    } // i < j
     if k < i {
         return i
     }
@@ -77,7 +119,18 @@ func medianThree(i, j, k int) int {
     return k
 }
 
-// maxDepth returnkvs a threshold at which quicksort should switch
+func maxThree(i, j, k int) int {
+    max := i
+    if max < j {
+        max = j
+    }
+    if max < k {
+        max = k
+    }
+    return max
+}
+
+// maxDepth returns a threshold at which quicksort should switch
 // to heapsort. It returnkvs 2*ceil(lg(n+1)).
 func maxDepth(n int) int {
     var depth int
@@ -87,8 +140,8 @@ func maxDepth(n int) int {
     return depth * 2
 }
 
-// siftDown implementkvs the heap property on kvs[lo:hi].
-// first ikvs an offset into the array where the root of the heap lies.
+// siftDown implements the heap property on kvs[lo:hi].
+// first is an offset into the array where the root of the heap lies.
 func siftDown(kvs kvSlice, lo, hi, first int) {
     root := lo
     for {
@@ -117,7 +170,7 @@ func heapSort(kvs kvSlice, a, b int) {
         siftDown(kvs, i, hi, first)
     }
 
-    // Pop elementkvs, largest first, into end of kvs.
+    // Pop elements, largest first, into end of kvs.
     for i := hi - 1; i >= 0; i-- {
         swap(kvs, first, first+i)
         siftDown(kvs, lo, i, first)
--- encoder/kvsort_test.go
+++ encoder/kvsort_test.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 ByteDance Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
 package encoder
 
 import (
@@ -83,7 +99,7 @@ func (x kvSlice) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
 func (x kvSlice) Len() int           { return len(x) }
 
 func TestSortRandKvs(t *testing.T) {
-    kvs := getRandKvs(100, 15)
+    kvs := getRandKvs(1000, 15)
     sorted := make([]keyValue, len(kvs))
 
     copy(sorted, kvs)
