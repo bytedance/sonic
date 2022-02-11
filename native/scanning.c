4a814a8bca74ba8be1e1368a96f8da390c1dbaf1
liuqiang <liuqiang.06@bytedance.com>
2022-02-11 15:30:15 +0800

    feat: add utf8 validate func

--- native/scanning.c
+++ native/scanning.c
@@ -134,6 +134,58 @@ static inline int _mm256_ascii_mask(__m256i vv) {
 
 #endif
 
+//      code point    | first byte | second byte | thrid byte | fourth byte
+//  U+0000  -  U+007F | 0___ ____
+//  U+0080  -  U+07FF | 110_ ____  | 10__ ____
+//  U+0800  -  U+FFFF | 1110 ____  | 10__ ____   | 10__ ____
+// U+10000 - U+10FFFF | 1111 0___  | 10__ ____   | 10__ ____  | 10__ ____
+// checks non-ascii characters, and returns the utf-8 length
+static inline size_t is_utf8(const uint8_t* noascii, size_t n) {
+#define is_next(b) (((b) >> 6) == 2)
+    uint8_t b0 = *noascii;
+    uint8_t b1 = *(noascii + 1);
+    uint8_t b2 = *(noascii + 2);
+    uint8_t b3 = *(noascii + 3);
+    uint32_t r;
+
+    /* 2-byte */
+    if (unlikely(n < 2 || !is_next(b1))) {
+        return 0;
+    }
+    if (unlikely(b0 < 0xe0 && b0 >= 0xc0)) {
+        r = ((uint32_t)(b0 & 0x1f) << 6) | (b1 & 0x3f);
+        if (likely(r >= 0x0080 && r <= 0x07ff)) {
+            return 2;
+        }
+        return 0;
+    }
+
+    /* 3-byte */
+    if (unlikely(n < 3 || !is_next(b2))) {
+        return 0;
+    }
+    if (likely(b0 < 0xf0)) {
+        r = ((uint32_t)(b0 & 0x0f) << 12) | ((uint32_t)(b1 & 0x3f) << 6) | (b2 & 0x3f);
+        if (likely(r >= 0x0800u && r <= 0xffffu)) {
+            return 3;
+        }
+        return 0;
+    }
+
+    /* 4-byte */
+    if (unlikely(n < 4 || !is_next(b3))) {
+        return 0;
+    }
+    if (likely(b0 < 0xf8)) {
+        r = ((uint32_t)(b0 & 0x07) << 18) | ((uint32_t)(b1 & 0x3f) << 12) | ((uint32_t)(b2 & 0x3f) << 6) | (b3 & 0x3f);
+        if (likely(r >= 0x10000u && r <= 0x10ffffu)) {
+            return 4;
+        }
+    }
+    return 0;
+#undef is_next
+}
+
 static inline ssize_t advance_string(const GoString *src, long p, int64_t *ep) {
     char     ch;
     uint64_t es;
@@ -342,7 +394,7 @@ static inline ssize_t advance_string(const GoString *src, long p, int64_t *ep) {
                 ep_setc()
                 sp++, nb--;
             }
-        } else if (unlikely( ch >= 0 && ch <= 0x1f)) {
+        } else if (unlikely( ch >= 0 && ch <= 0x1f)) { // control chars
             ep_setc()
             return -ERR_INVAL;
         }
@@ -402,7 +454,7 @@ void vstring(const GoString *src, long *p, JsonState *ret) {
 
     /* check for errors */
     if (e < 0) {
-        *p = src->len;
+        *p = e == -ERR_EOF ? src->len : v;
         ret->vt = e;
         return;
     }
