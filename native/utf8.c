b2f6a948d3cf57db5211c13e641c98f4a498e8db
liuqiang <liuqiang.06@bytedance.com>
2022-08-08 11:31:45 +0800

    update license

--- native/utf8.c
+++ native/utf8.c
@@ -1,11 +1,21 @@
-/*==============================================================================
- * Created by Yaoyuan on 2019/3/9.
+/*
  * Copyright (C) 2019 Yaoyuan <ibireme@gmail.com>.
- * Modifications copyright 2022 ByteDance Inc.
- * 
- * Released under the MIT License:
- * https://github.com/ibireme/yyjson/blob/master/LICENSE
- *============================================================================*/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This file may have been modified by ByteDance authors. All ByteDance
+ * Modifications are Copyright 2022 ByteDance Authors.
+ */
 
 static inline ssize_t valid_utf8_4byte(uint32_t ubin) {
     /*
