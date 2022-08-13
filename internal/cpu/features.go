3799a65fa199eb90638bb97362f029aea9893573
cyn6315 <631574231@qq.com>
2022-08-13 11:00:13 +0800

    remove excessive spaces

--- internal/cpu/features.go
+++ internal/cpu/features.go
@@ -14,28 +14,28 @@
  * limitations under the License.
  */
 
- package cpu
+package cpu
 
- import (
-	 `fmt`
-	 `os`
+import (
+	`fmt`
+	`os`
  
-	 `github.com/klauspost/cpuid/v2`
- )
+	`github.com/klauspost/cpuid/v2`
+)
  
- var (
-	 HasAVX  = cpuid.CPU.Has(cpuid.AVX)
-	 HasAVX2 = cpuid.CPU.Has(cpuid.AVX2)
-	 HasSSE4 = cpuid.CPU.Has(cpuid.SSE4)
- )
+var (
+	HasAVX  = cpuid.CPU.Has(cpuid.AVX)
+	HasAVX2 = cpuid.CPU.Has(cpuid.AVX2)
+	HasSSE4 = cpuid.CPU.Has(cpuid.SSE4)
+)
  
- func init() {
-	 switch v := os.Getenv("SONIC_MODE"); v {
-		 case ""       : break
-		 case "auto"   : break
-		 case "noavx"  : HasAVX = false; fallthrough
-		 case "noavx2" : HasAVX2 = false
-		 default       : panic(fmt.Sprintf("invalid mode: '%s', should be one of 'auto', 'noavx', 'noavx2'", v))
+func init() {
+	switch v := os.Getenv("SONIC_MODE"); v {
+		case ""       : break
+		case "auto"   : break
+		case "noavx"  : HasAVX = false; fallthrough
+		case "noavx2" : HasAVX2 = false
+		default       : panic(fmt.Sprintf("invalid mode: '%s', should be one of 'auto', 'noavx', 'noavx2'", v))
 	 }
- }
+}
  
