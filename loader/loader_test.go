9d92aa584592e1864fc58113bb95fe226a5f9e87
duanyi.aster <duanyi.aster@bytedance.com>
2023-02-07 15:30:04 +0800

    add test

--- loader/loader_test.go
+++ loader/loader_test.go
@@ -1,4 +1,5 @@
-// +build go1.15,!go1.17
+//go:build go1.17
+// +build go1.17
 
 /*
  * Copyright 2021 ByteDance Inc.
@@ -19,24 +20,115 @@
 package loader
 
 import (
-    `runtime`
-    `testing`
-    
-    `github.com/stretchr/testify/assert`
+	"runtime"
+	"runtime/debug"
+	"strconv"
+	"testing"
+	"unsafe"
+
+	"github.com/bytedance/sonic/internal/rt"
+	"github.com/stretchr/testify/require"
 )
 
-func TestLoader_LoadFunc(t *testing.T) {
+func TestLoad(t *testing.T) {
+    // defer func() {
+    //     if r := recover(); r != nil {
+    //         runtime.GC()
+    //         if r != "hook1" {
+    //             t.Fatal("not right panic:" + r.(string))
+    //         }
+    //     } else {
+    //         t.Fatal("not panic")
+    //     }
+    // }()
+
+    var hstr string
+
+    type TestFunc func(i *int, hook func(i *int)) int
+    var hook = func(i *int) {
+        runtime.GC()
+        debug.FreeOSMemory()
+        hstr = ("hook" + strconv.Itoa(*i))
+        runtime.GC()
+        debug.FreeOSMemory()
+    }
+    // var f TestFunc = func(i *int, hook func(i *int)) int {
+    //     var t = *i
+    //     hook(i)
+    //     return t + *i
+    // }
     bc := []byte {
-        0x48, 0x8b, 0x44, 0x24, 0x08,               // MOVQ  8(%rsp), %rax
-        0x48, 0xc7, 0x00, 0xd2, 0x04, 0x00, 0x00,   // MOVQ  $1234, (%rax)
-        0xc3,                                       // RET
+        0x48, 0x83, 0xec, 0x18,         // (0x00) subq $24, %rsp
+        0x48, 0x89, 0x6c, 0x24, 0x10,   // (0x04) movq %rbp, 16(%rsp)
+        0x48, 0x8d, 0x6c, 0x24, 0x10,   // (0x09) leaq 16(%rsp), %rbp
+        0x48, 0x89, 0x44, 0x24, 0x20,   // (0x0e) movq %rax, 32(%rsp)
+        0x48, 0x8b, 0x08,               // (0x13) movq (%rax), %rcx
+        0x48, 0x89, 0x4c, 0x24, 0x08,   // (0x16) movq %rcx, 8(%rsp)
+        0x48, 0x8b, 0x33,               // (0x1b) movq (%rbx), %rsi
+        0x48, 0x89, 0xda,               // (0x1e) movq %rbx, %rdx
+        0xff, 0xd6,                     // (0x21) callq %rsi
+        0x48, 0x8b, 0x44, 0x24, 0x08,   // (0x23) movq 8(%rsp), %rax
+        0x48, 0x8b, 0x4c, 0x24, 0x20,   // (0x28) movq 32(%rsp), %rcx
+        0x48, 0x03, 0x01,               // (0x2d) addq (%rcx), %rax
+        0x48, 0x8b, 0x6c, 0x24, 0x10,   // (0x30) movq 16(%rsp), %rbp
+        0x48, 0x83, 0xc4, 0x18,         // (0x35) addq $24, %rsp
+        0xc3,                           // (0x39) ret
     }
-    v0 := 0
-    l := ModuleLoader{
-        Name: "test",
-        File: "test.go",
+    size := uint32(len(bc))
+    fn := Func{
+        ID: 0,
+        Flag: 0,
+        ArgsSize: 16,
+        EntryOff: 0,
+        TextSize: size,
+        DeferReturn: 0,
+        FileIndex: 0,
+        Name: "dummy",
     }
-    fn := l.LoadFunc(bc, "test", 0, 8, []bool{true}, []bool{})
-    assert.Equal(t, 1234, v0)
-    println(runtime.FuncForPC(*(*uintptr)(fn)).Name())
+
+    fn.Pcsp = &Pcdata{
+        {PC: 0x04, Val: 0},
+        {PC: size, Val: 24},
+    }
+
+    fn.Pcline = &Pcdata{
+        {PC: 0x00, Val: 0},
+        {PC: 0x0e, Val: 1},
+        {PC: 0x1d, Val: 2},
+        {PC: size, Val: 3},
+    }
+
+    fn.Pcfile = &Pcdata{
+        {PC: size, Val: 0},
+    }
+
+    fn.PcUnsafePoint = &Pcdata{
+        {PC: size, Val: PCDATA_UnsafePointUnsafe},
+    }
+
+    fn.PcStackMapIndex = &Pcdata{
+        {PC: size, Val: 0},
+    }
+
+    args := rt.StackMapBuilder{}
+    args.AddField(true)
+    args.AddField(true)
+    fn.ArgsPointerMaps = args.Build()
+
+    locals := rt.StackMapBuilder{}
+    locals.AddField(false)
+    locals.AddField(false)
+    fn.LocalsPointerMaps = locals.Build()
+
+    rets := Load("dummy_module", []string{"github.com/bytedance/sonic/dummy.go"}, []Func{fn}, bc)
+    println("func address ", *(*unsafe.Pointer)(rets[0]))
+    // for k, _ := range moduleCache.m {
+    //     spew.Dump(k)
+    // }
+
+    f := *(*TestFunc)(unsafe.Pointer(&rets[0]))
+    i := 1
+    j := f(&i, hook)
+    require.Equal(t, 2, j)
+    require.Equal(t, "hook1", hstr)
 }
