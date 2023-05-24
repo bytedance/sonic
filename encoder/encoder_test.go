c8c898d3cb5cc816cb8441b766b38398396bca4f
liuq19 <liuqiang2016@gmail.com>
2023-05-24 18:40:41 +0800

    fix: remove unused codes

--- encoder/encoder_test.go
+++ encoder/encoder_test.go
@@ -84,22 +84,6 @@ type sample struct {
     AP *[0]interface{}
 }
 
-// func TestOptionSliceOrMapNoNull(t *testing.T) {
-//     obj := sample{}
-//     out, err := Encode(obj, NoNullSliceOrMap)
-//     if err != nil {
-//         t.Fatal(err)
-//     }
-//     require.Equal(t, `{"M":{},"S":[],"A":[],"MP":null,"SP":null,"AP":null}`, string(out))
-
-//     obj2 := sample{}
-//     out, err = Encode(obj2, 0)
-//     if err != nil {
-//         t.Fatal(err)
-//     }
-//     require.Equal(t, `{"M":null,"S":null,"A":[],"MP":null,"SP":null,"AP":null}`, string(out))
-// }
-
 func BenchmarkOptionSliceOrMapNoNull(b *testing.B) {
     b.Run("true", func (b *testing.B) {
         obj := sample{}
@@ -179,23 +163,6 @@ type MarshalerStruct struct {
     V MarshalerImpl
 }
 
-// func TestEncoder_Marshaler(t *testing.T) {
-//     v := MarshalerStruct{V: MarshalerImpl{X: 12345}}
-//     ret, err := Encode(&v, 0)
-//     require.NoError(t, err)
-//     require.Equal(t, `{"V":12345    }`, string(ret))
-//     ret, err = Encode(v, 0)
-//     require.NoError(t, err)
-//     require.Equal(t, `{"V":{"X":12345}}`, string(ret))
-
-//     ret2, err2 := Encode(&v, 0)
-//     require.NoError(t, err2)
-//     require.Equal(t, `{"V":12345    }`, string(ret2))
-//     ret3, err3 := Encode(v, CompactMarshaler)
-//     require.NoError(t, err3)
-//     require.Equal(t, `{"V":{"X":12345}}`, string(ret3))
-// }
-
 type MarshalerErrorStruct struct {
     V MarshalerImpl
 }
@@ -204,29 +171,9 @@ func (self *MarshalerErrorStruct) MarshalJSON() ([]byte, error) {
     return []byte(`[""] {`), nil
 }
 
-// func TestMarshalerError(t *testing.T) {
-//     v := MarshalerErrorStruct{}
-//     ret, err := Encode(&v, 0)
-//     require.EqualError(t, err, `invalid Marshaler output json syntax at 5: "[\"\"] {"`)
-//     require.Equal(t, []byte(nil), ret)
-// }
-
 type RawMessageStruct struct {
     X json.RawMessage
 }
-
-// func TestEncoder_RawMessage(t *testing.T) {
-//     rms := RawMessageStruct{
-//         X: json.RawMessage("123456    "),
-//     }
-//     ret, err := Encode(&rms, 0)
-//     require.NoError(t, err)
-//     require.Equal(t, `{"X":123456    }`, string(ret))
-
-//     ret, err = Encode(&rms, CompactMarshaler)
-//     require.NoError(t, err)
-//     require.Equal(t, `{"X":123456}`, string(ret))
-// }
 
 type TextMarshalerImpl struct {
     X string
@@ -247,23 +194,6 @@ func (self TextMarshalerImplV) MarshalText() ([]byte, error) {
 type TextMarshalerStruct struct {
     V TextMarshalerImpl
 }
-
-// func TestEncoder_TextMarshaler(t *testing.T) {
-//     v := TextMarshalerStruct{V: TextMarshalerImpl{X: (`{"a"}`)}}
-//     ret, err := Encode(&v, 0)
-//     require.NoError(t, err)
-//     require.Equal(t, `{"V":"{\"a\"}"}`, string(ret))
-//     ret, err = Encode(v, 0)
-//     require.NoError(t, err)
-//     require.Equal(t, `{"V":{"X":"{\"a\"}"}}`, string(ret))
-
-//     ret2, err2 := Encode(&v, NoQuoteTextMarshaler)
-//     require.NoError(t, err2)
-//     require.Equal(t, `{"V":{"a"}}`, string(ret2))
-//     ret3, err3 := Encode(v, NoQuoteTextMarshaler)
-//     require.NoError(t, err3)
-//     require.Equal(t, `{"V":{"X":"{\"a\"}"}}`, string(ret3))
-// }
 
 func TestTextMarshalTextKey_SortKeys(t *testing.T) {
     v := map[*TextMarshalerImpl]string{
@@ -293,28 +223,6 @@ func TestTextMarshalTextKey_SortKeys(t *testing.T) {
     require.NoError(t, err)
     require.Equal(t, `{"a":"a","b":"b","c":"c"}`, string(ret))
 }
-
-// func TestEncoder_Marshal_EscapeHTML(t *testing.T) {
-//     v := map[string]TextMarshalerImpl{"&&":{"<>"}}
-//     ret, err := Encode(v, EscapeHTML)
-//     require.NoError(t, err)
-//     require.Equal(t, `{"\u0026\u0026":{"X":"\u003c\u003e"}}`, string(ret))
-//     ret, err = Encode(v, 0)
-//     require.NoError(t, err)
-//     require.Equal(t, `{"&&":{"X":"<>"}}`, string(ret))
-
-//     // “ is \xe2\x80\x9c, and ” is \xe2\x80\x9d,
-//     // similar as HTML escaped chars \u2028(\xe2\x80\xa8) and \u2029(\xe2\x80\xa9)
-//     m := map[string]string{"test": "“123”"}
-//     ret, err = Encode(m, EscapeHTML)
-//     require.Equal(t, string(ret), `{"test":"“123”"}`)
-//     require.NoError(t, err)
-
-//     m = map[string]string{"K": "\u2028\u2028\xe2"}
-//     ret, err = Encode(m, EscapeHTML)
-//     require.Equal(t, string(ret), "{\"K\":\"\\u2028\\u2028\xe2\"}")
-//     require.NoError(t, err)
-// }
 
 func TestEncoder_EscapeHTML(t *testing.T) {
     // test data from libfuzzer
@@ -369,8 +277,6 @@ func TestEncoder_MapSortKey(t *testing.T) {
         "B": "second",
     }
     v, e := Encode(m, SortMapKeys)
-    out, _ := json.Marshal(m)
-    println(string(out))
     require.NoError(t, e)
     require.Equal(t, `{"A":"first","B":"second","C":"third","D":"forth","E":"fifth","F":"sixth"}`, string(v))
 }
