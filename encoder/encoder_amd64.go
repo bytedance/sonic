71b1999d2809bb49f8cfd308f148e98fccc0b182
liuqiang.06 <liuqiang.06@bytedance.com>
2023-05-24 16:58:31 +0800

    fix: support fallback in encoder/decoder

--- encoder/encoder_amd64.go
+++ encoder/encoder_amd64.go
@@ -1,30 +1,26 @@
 // +build amd64,go1.15,!go1.21
 
+/*
+ * Copyright 2023 ByteDance Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package encoder
 
 import (
     `github.com/bytedance/sonic/internal/encoder`
 )
-
-// func Encode(val interface{}, opts Options) ([]byte, error)
-// func EncodeIndented(val interface{}, prefix string, indent string, opts Options) ([]byte, error)
-// func EncodeInto(buf *[]byte, val interface{}, opts Options) error
-// func HTMLEscape(dst []byte, src []byte) []byte
-// func Pretouch(vt reflect.Type, opts ...option.CompileOption) error
-// func Quote(s string) string
-// func Valid(data []byte) (ok bool, start int)
-// type Encoder
-// func (self *Encoder) Encode(v interface{}) ([]byte, error)
-// func (self *Encoder) SetCompactMarshaler(f bool)
-// func (self *Encoder) SetEscapeHTML(f bool)
-// func (enc *Encoder) SetIndent(prefix, indent string)
-// func (self *Encoder) SetNoQuoteTextMarshaler(f bool)
-// func (self *Encoder) SetValidateString(f bool)
-// func (self *Encoder) SortKeys() *Encoder
-// type Options
-// type StreamEncoder
-// func NewStreamEncoder(w io.Writer) *StreamEncoder
-// func (enc *StreamEncoder) Encode(val interface{}) (err error)
 
 var (
     Encode = encoder.Encode
@@ -41,34 +37,34 @@ type Encoder = encoder.Encoder
 type Options = encoder.Options
 
 const (
-	// SortMapKeys indicates that the keys of a map needs to be sorted
-	// before serializing into JSON.
-	// WARNING: This hurts performance A LOT, USE WITH CARE.
-	SortMapKeys Options = encoder.SortMapKeys
+    // SortMapKeys indicates that the keys of a map needs to be sorted
+    // before serializing into JSON.
+    // WARNING: This hurts performance A LOT, USE WITH CARE.
+    SortMapKeys Options = encoder.SortMapKeys
 
-	// EscapeHTML indicates encoder to escape all HTML characters
-	// after serializing into JSON (see https://pkg.go.dev/encoding/json#HTMLEscape).
-	// WARNING: This hurts performance A LOT, USE WITH CARE.
-	EscapeHTML Options = encoder.EscapeHTML
+    // EscapeHTML indicates encoder to escape all HTML characters
+    // after serializing into JSON (see https://pkg.go.dev/encoding/json#HTMLEscape).
+    // WARNING: This hurts performance A LOT, USE WITH CARE.
+    EscapeHTML Options = encoder.EscapeHTML
 
-	// CompactMarshaler indicates that the output JSON from json.Marshaler
-	// is always compact and needs no validation
-	CompactMarshaler Options = encoder.CompactMarshaler
+    // CompactMarshaler indicates that the output JSON from json.Marshaler
+    // is always compact and needs no validation
+    CompactMarshaler Options = encoder.CompactMarshaler
 
-	// NoQuoteTextMarshaler indicates that the output text from encoding.TextMarshaler
-	// is always escaped string and needs no quoting
-	NoQuoteTextMarshaler Options = encoder.NoQuoteTextMarshaler
+    // NoQuoteTextMarshaler indicates that the output text from encoding.TextMarshaler
+    // is always escaped string and needs no quoting
+    NoQuoteTextMarshaler Options = encoder.NoQuoteTextMarshaler
 
-	// NoNullSliceOrMap indicates all empty Array or Object are encoded as '[]' or '{}',
-	// instead of 'null'
-	NoNullSliceOrMap Options = encoder.NoNullSliceOrMap
+    // NoNullSliceOrMap indicates all empty Array or Object are encoded as '[]' or '{}',
+    // instead of 'null'
+    NoNullSliceOrMap Options = encoder.NoNullSliceOrMap
 
-	// ValidateString indicates that encoder should validate the input string
-	// before encoding it into JSON.
-	ValidateString Options = encoder.ValidateString
+    // ValidateString indicates that encoder should validate the input string
+    // before encoding it into JSON.
+    ValidateString Options = encoder.ValidateString
 
-	// CompatibleWithStd is used to be compatible with std encoder.
-	CompatibleWithStd Options = encoder.CompatibleWithStd
+    // CompatibleWithStd is used to be compatible with std encoder.
+    CompatibleWithStd Options = encoder.CompatibleWithStd
 )
 
 type StreamEncoder = encoder.StreamEncoder
