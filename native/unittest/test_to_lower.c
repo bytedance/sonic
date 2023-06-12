1eff5c3f8a9152627c4fc05372a794e99f2b0c69
gaodaiwei <gaodaiwei2@gmail.com>
2023-06-12 18:56:22 +0800

    test & makefile

--- native/unittest/test_to_lower.c
+++ native/unittest/test_to_lower.c
@@ -3,13 +3,14 @@
 
 void test_to_lower(const char* input, const char* expect) {
     int len = sizeof(input);
-    char* dst = (char*)malloc(len + 1);
+    char* dst = (char*)malloc(len);
     to_lower(dst, input, len);
     assert(strncmp(expect, dst) == 0);
+    free(dst);
 }
 
 int main() {
     test_to_lower("Hello, World!", "hello, world!");
     test_to_lower("12345", "12345");
-    test_to_lower("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    test_to_lower("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz");
 }
