75a8fc1cad39e115fef35420b9256f331cf9d8f1
lijinnan123 <1760485547@qq.com>
2023-06-24 21:18:32 +0800

    add license and unittests

--- native/hashmap_get.c
+++ native/hashmap_get.c
@@ -8,8 +8,10 @@ int64_t field_hashmap_get(FieldHashMap *fmap, const GoString* key){
     XXH64_hash_t seed   =    123456789;
     size_t len          =    fmap->N;
     uint64_t hash       =    XXH64(key->name, len, seed);
+    int64_t  id         =    -1;
 
     if(fmap.bucket[hash] != NULL){
-        return fmap.bucket[hash]->id;
+        id              =    fmap.bucket[hash]->id;
     }
+    return id;
 }
---
+++ native/unittest/test_hashgetset.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "../native.h"
+#include "../hashmap_get.c"
+#include "../hashmap_set.c"
+
+int main() {
+    FieldHashMap map;
+    map.N      = 10000;
+    map.bucket = NULL;
+
+    GoString key1;
+    GoString key2;
+    GoString key3;
+
+    int64_t value1;
+    int64_t value2;
+    int64_t value3;
+
+    key1.buf   = "Hello";
+    key1.len   = 5;
+    key2.buf   = "World";
+    key2.len   = 5;
+    key3.buf   = "!!!!";
+    key3.len   = 4;
+
+    field_hashmap_set(&map, &key1, 1);
+    field_hashmap_set(&map, &key2, 2);
+    value1 = field_hashmap_get(&map, &key1);
+    value2 = field_hashmap_get(&map, &key2);
+    printf("The value1 is: %" PRId64 "\n", value1);
+    printf("The value2 is: %" PRId64 "\n", value2);
+    
+    value3 = field_hashmap_get(&map, &key3);
+    printf("The value3 is: %" PRId64 "\n", value3);
+    field_hashmap_set(&map, &key3, 3);
+    value3 = field_hashmap_get(&map, &key3);
+    printf("The value3 is: %" PRId64 "\n", value3);
+}
+
+
+
+
