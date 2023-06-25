570a967604610557f8e32bb09b9c3f654029af30
dongchao <hhu_dc@163.com>
2023-06-25 14:25:47 +0800

    unittest

--- native/unittest/unittest_goIntSlice.c
+++ native/unittest/unittest_goIntSlice.c
@@ -4,10 +4,10 @@
 #include "../goIntSlice.c"
 
 void test_decode_i64_array(const char* test_str){
-	GoIntSlice demo;
+    GoIntSlice demo;
     size_t cap = 4096;
     demo.iptr = (int64_t*)malloc(cap * sizeof(int64_t));
-	demo.len = 0;
+    demo.len = 0;
     demo.cap = cap;
     
     long p = 0;
@@ -18,9 +18,9 @@ void test_decode_i64_array(const char* test_str){
     test.buf = str;
     test.len = strlen(str);
 	
-	long res = decode_i64_array(&test,pp,&demo);
+    long res = decode_i64_array(&test,pp,&demo);
     
-	printf("%lld ,%lld\n",*pp,res);
+    printf("%lld ,%lld\n",*pp,res);
 	
     for(int z=0;z<demo.len;z++){
     	printf(" %d ",demo.iptr[z]);
@@ -33,10 +33,10 @@ void test_decode_i64_array(const char* test_str){
 }
 
 void test_decode_u64_array(const char* test_str){
-	GoIntSlice demo;
+    GoIntSlice demo;
     size_t cap = 4096;
     demo.uptr = (uint64_t*)malloc(cap * sizeof(uint64_t));
-	demo.len = 0;
+    demo.len = 0;
     demo.cap = cap;
     
     long p = 0;
@@ -47,9 +47,9 @@ void test_decode_u64_array(const char* test_str){
     test.buf = str;
     test.len = strlen(str);
 	
-	long res = decode_u64_array(&test,pp,&demo);
+    long res = decode_u64_array(&test,pp,&demo);
     
-	printf("%lld ,%lld\n",*pp,res);
+    printf("%lld ,%lld\n",*pp,res);
 	
     for(int z=0;z<demo.len;z++){
     	printf(" %d ",demo.uptr[z]);
@@ -61,19 +61,6 @@ void test_decode_u64_array(const char* test_str){
 
 int main(){
     
-    /*
-    目前测试用例满足的有：
-	 "{[1,2,3,4]"  错误用例 
-	 "[1,2,3.5,4]"  错误用例
-     "[1,  2,3, 4]"  正确用例但是有不规则空格
-     "[  1,2,3,4]"   正确用例但是有不规则空格
-	 "[1,2,3,4]"  正确用例 
-     "[1,2,3]"    正确用例 
-     "[1  ,2,3,4]"   正确用例但是有不规则空格
-     "[1,-2,-3,4]"  正确用例 
-     "[1,-2,  -3, 4]"   正确用例但是有不规则空格
-     "[1, -2.3, 4]"     错误用例 
-    */
     
     char teststr[10][100] = {"{[1,2,3,4]","[1,2,3.5,4]","[1,  2,3, 4]","[  1,2,3,4]","[1,2,3,4]","[1,2,3]","[1  ,2,3,4]","[1,-2,-3,4]","[1,-2,  -3, 4]","[1, -2.3, 4]"};
     
@@ -83,12 +70,7 @@ int main(){
     	test_decode_i64_array(teststr[i]);
     	printf("*");
 	}
-    
-    
-   
-    
-	
-	
+    	
 	
     
 }
