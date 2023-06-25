2a63c21b92dad5ecfd96d06418dd76f59514d604
dongchao <hhu_dc@163.com>
2023-06-25 13:22:18 +0800

    unittest

---
+++ native/unittest/unittest_goIntSlice.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "../goIntSlice.c"
+
+void test_decode_i64_array(const char* test_str){
+	GoIntSlice demo;
+    size_t cap = 4096;
+    demo.iptr = (int64_t*)malloc(cap * sizeof(int64_t));
+	demo.len = 0;
+    demo.cap = cap;
+    
+    long p = 0;
+    long* pp = &p; 
+    GoString test;
+    
+    const char* str = test_str; 
+    test.buf = str;
+    test.len = strlen(str);
+	
+	long res = decode_i64_array(&test,pp,&demo);
+    
+	printf("%lld ,%lld\n",*pp,res);
+	
+    for(int z=0;z<demo.len;z++){
+    	printf(" %d ",demo.iptr[z]);
+    	
+	}
+	
+	free(demo.iptr);
+	
+	
+}
+
+void test_decode_u64_array(const char* test_str){
+	GoIntSlice demo;
+    size_t cap = 4096;
+    demo.uptr = (uint64_t*)malloc(cap * sizeof(uint64_t));
+	demo.len = 0;
+    demo.cap = cap;
+    
+    long p = 0;
+    long* pp = &p; 
+    GoString test;
+    
+    const char* str = test_str; 
+    test.buf = str;
+    test.len = strlen(str);
+	
+	long res = decode_u64_array(&test,pp,&demo);
+    
+	printf("%lld ,%lld\n",*pp,res);
+	
+    for(int z=0;z<demo.len;z++){
+    	printf(" %d ",demo.uptr[z]);
+    	
+	}
+	
+	free(demo.uptr);
+}
+
+int main(){
+    
+    /*
+    目前测试用例满足的有：
+	 "{[1,2,3,4]"  错误用例 
+	 "[1,2,3.5,4]"  错误用例
+     "[1,  2,3, 4]"  正确用例但是有不规则空格
+     "[  1,2,3,4]"   正确用例但是有不规则空格
+	 "[1,2,3,4]"  正确用例 
+     "[1,2,3]"    正确用例 
+     "[1  ,2,3,4]"   正确用例但是有不规则空格
+     "[1,-2,-3,4]"  正确用例 
+     "[1,-2,  -3, 4]"   正确用例但是有不规则空格
+     "[1, -2.3, 4]"     错误用例 
+    */
+    
+    char teststr[10][100] = {"{[1,2,3,4]","[1,2,3.5,4]","[1,  2,3, 4]","[  1,2,3,4]","[1,2,3,4]","[1,2,3]","[1  ,2,3,4]","[1,-2,-3,4]","[1,-2,  -3, 4]","[1, -2.3, 4]"};
+    
+    for(int i=0;i<10;i++){
+    	test_decode_u64_array(teststr[i]);
+    	printf("*");
+    	test_decode_i64_array(teststr[i]);
+    	printf("*");
+	}
+    
+    
+   
+    
+	
+	
+	
+    
+}
