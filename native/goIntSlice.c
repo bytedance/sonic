a9743c7e952e492ae1cb2c17db79672fb749589b
dongchao <hhu_dc@163.com>
2023-06-26 22:48:10 +0800

    latest modified

--- native/goIntSlice.c
+++ native/goIntSlice.c
@@ -23,7 +23,7 @@ typedef struct {
 
 
 bool isSpace(char a){
-    if( a == ' '){
+    if( a == ' '||a == '\t'||a == '\r'||a == '\n'){
         return true;
     }else{
         return false;
@@ -31,15 +31,15 @@ bool isSpace(char a){
 }
 
 bool isInteger(char a){
-	if(a<'0' ||a>'9'){
-		return false;
-	}else{
-		return true;
-	}
+    if(a<'0' ||a>'9'){
+	return false;
+    }else{
+	return true;
+    }
 }
 
-int charToNum(char c){
-	return c-'0';
+int char_to_num(char c){
+    return c-'0';
 }
 
 
@@ -48,7 +48,7 @@ long decode_u64_array( const GoString* src, long* p, GoIntSlice* arr){
     int i =0;
     
     while(isSpace(pos[i])){                                          //If there is a space before the beginning, eat the space first 
-    	i++;                           
+        i++;                           
 	}
     if(pos[i] != '['){                                               //If the first one is not a left bracket, returning it directly is illegal
         *p = i+1;                                                    //P points to the first position after the error 
@@ -60,74 +60,74 @@ long decode_u64_array( const GoString* src, long* p, GoIntSlice* arr){
     int num = 0;                                                     //Num is used to store the current number 
     while(pos[i] !='\0'){
         if(k==arr->cap){                                             //If the capacity is insufficient, return ERR_ RECURSE_ MAX
-        	*p = i+1;
+            *p = i+1;
             return ERR_RECURSE_MAX;
         }
         while(isSpace(pos[i])){                                      //Jump back if it's a space 
-        	i++;             
-		}
-		if(pos[i]<'0' || pos[i]>'9'){
-			*p = i+1;                                    //P points to the first position after the error 
+            i++;             
+	    }
+	    if(pos[i]<'0' || pos[i]>'9'){
+		*p = i+1;                                    //P points to the first position after the error 
         	arr->len = 0;
-			return ERR_INVAL;                            //The first one is not a number, it must be illegal 
-		}else{
-			num = charToNum(pos[i]); 
-		} 
+		return ERR_INVAL;                            //The first one is not a number, it must be illegal 
+	    }else{
+		num = char_to_num(pos[i]); 
+	    } 
 		    
-		i++; 
-		while(!isSpace(pos[i])&& pos[i] !=','){                         
+	    i++; 
+	    while(!isSpace(pos[i])&& pos[i] !=','){                         
 		 	  
-			//If it is not followed by a space or a comma, it indicates that it is a number or an error has occurred
-			 if(isInteger(pos[i])){		 		
-		 		num = num*10 + charToNum(pos[i]);
-        		i++;		
-			}else if(pos[i] ==']'){                      //If the right bracket is used, it can be closed and a value can be returned 
-				(arr->uptr)[k] = num;
-				arr->len = k+1;
-				*p = i+1;
-				return 0;
-			}else{
+	    //If it is not followed by a space or a comma, it indicates that it is a number or an error has occurred
+	    if(isInteger(pos[i])){		 		
+	        num = num*10 + char_to_num(pos[i]);
+        	i++;		
+	    }else if(pos[i] ==']'){                      //If the right bracket is used, it can be closed and a value can be returned 
+		(arr->uptr)[k] = num;
+		arr->len = k+1;
+		*p = i+1;
+		return 0;
+	    }else{
 			
-			//If it's not a number or a comma, it's an error. Point to the position after the error and return ERR_ INVAL 
-				*p = i+1;                                               
-        		arr->len = 0;
-        		return ERR_INVAL;
-			} 
-		}
-		while(isSpace(pos[i])){
-			i++;
-		}
-		if(pos[i] ==']'){
-			(arr->uptr)[k] = num;
-				arr->len = k+1;
-				*p = i+1;
-				return 0;
-		}
-		if(isInteger(pos[i])){
-			*p = i+1;
-			arr->len = 0;
-			return ERR_INVAL;
-		}
-		if(pos[i] ==','){                                                //If it's a comma, put it away 
-			(arr->uptr)[k] = num; 
-			k++;
-			i++;
-		}
-		if(pos[i] =='\0'){
-			*p = i+1;
-			arr->len = 0;
-			return ERR_INVAL;
-		}			 			             
+	    //If it's not a number or a comma, it's an error. Point to the position after the error and return ERR_ INVAL 
+	        *p = i+1;                                               
+        	arr->len = 0;
+        	return ERR_INVAL;
+	    } 
+	}
+	    while(isSpace(pos[i])){
+	    i++;
+	    }
+	    if(pos[i] ==']'){
+		(arr->uptr)[k] = num;
+		arr->len = k+1;
+		*p = i+1;
+		return 0;
+	    }
+	    if(isInteger(pos[i])){
+	        *p = i+1;
+		arr->len = 0;
+		return ERR_INVAL;
+	    }
+	    if(pos[i] ==','){                                                //If it's a comma, put it away 
+		(arr->uptr)[k] = num; 
+		k++;
+		i++;
+	    }
+	    if(pos[i] =='\0'){
+		*p = i+1;
+		arr->len = 0;
+		return ERR_INVAL;
+	    }			 			             
     }
 }
 
 long decode_i64_array(const GoString* src, long* p, GoIntSlice* arr){   
-	char* pos = src->buf;
-	int i =0;
+    char* pos = src->buf;
+    int i =0;
 	    
-	while(isSpace(pos[i])){                                             
-	    i++;                           
-	} 
+    while(isSpace(pos[i])){                                             
+        i++;                           
+    } 
     if(pos[i] != '['){                                                  
         *p = i+1;                                                       
         arr->len = 0;
@@ -139,77 +139,72 @@ long decode_i64_array(const GoString* src, long* p, GoIntSlice* arr){
     char flag ='+';                                              //Define a flag to represent the symbol of a signed number
     while(pos[i] !=0){
         if(k==arr->cap){                                                
-        	*p = i+1;
+            *p = i+1;
             return ERR_RECURSE_MAX;
         }
         while(isSpace(pos[i])){                                         
-        	i++;             
-		}
-		if((pos[i]<'0' || pos[i]>'9') && (pos[i]!='+' && pos[i]!='-' )){
+            i++;             
+	}
+	if((pos[i]<'0' || pos[i]>'9') && (pos[i]!='+' && pos[i]!='-' )){
 			
-			*p = i+1;                                                   
+	    *p = i+1;                                                   
+            arr->len = 0;
+	    return ERR_INVAL;                        //The first one is neither a number nor a Plus¨Cminus sign, so it must be illegal 
+	}else if(pos[i]=='+' || pos[i]=='-'){
+			
+	    //If it is a symbol, then the symbol is fixed, and the first digit immediately after it is also stored in num and i+1 
+	    flag =  pos[i];                           
+	    i++;
+	    num = char_to_num(pos[i]);			
+	}else{
+	    num = char_to_num(pos[i]);            
+	} 	    
+	i++; 
+	while(!isSpace(pos[i])&& pos[i] !=','){                          
+	    if(isInteger(pos[i])){
+	        num = num*10 + char_to_num(pos[i]);
+        	i++;		
+	    }else if(pos[i] ==']'){                                     
+	        if(flag =='-'){
+		    num = -(num);
+		}
+		(arr->iptr)[k] = num;
+		arr->len = k+1;
+		*p = i+1;
+		return 0;
+	    }else{
+		*p = i+1;                                                
         	arr->len = 0;
-			return ERR_INVAL;                        //The first one is neither a number nor a Plus¨Cminus sign, so it must be illegal 
-		}else if(pos[i]=='+' || pos[i]=='-'){
-			
-		//If it is a symbol, then the symbol is fixed, and the first digit immediately after it is also stored in num and i+1 
-			flag =  pos[i];                           
-			i++;
-			num = charToNum(pos[i]);
-			
-		}else{
-			num = charToNum(pos[i]);            
+        	return ERR_INVAL;
 		} 
-		    
-		i++; 
-		while(!isSpace(pos[i])&& pos[i] !=','){                          
-		 	if(isInteger(pos[i])){
-		 		
-		 		num = num*10 + charToNum(pos[i]);
-        		i++;		
-			}else if(pos[i] ==']'){                                     
-			    if(flag =='-'){
-			    	num = -(num);
-				}
-				(arr->iptr)[k] = num;
-				arr->len = k+1;
-				*p = i+1;
-				return 0;
-			}else{
-				*p = i+1;                                                
-        		arr->len = 0;
-        		return ERR_INVAL;
-			} 
-		} 
-		while(isSpace(pos[i])){
-			i++;
-		}
-		if(pos[i] ==']'){
-			(arr->uptr)[k] = num;
-				arr->len = k+1;
-				*p = i+1;
-				return 0;
-		}
-		if(isInteger(pos[i])){
-			*p = i+1;
-			arr->len = 0;
-			return ERR_INVAL;
-		}
-		if(pos[i] ==','){                                                
-			
-			if(flag =='-'){
-			    num = -(num);
-			}
-			(arr->iptr)[k] = num; 
-			k++;
-			i++;
-		}
-		if(pos[i] =='\0'){
-			*p = i+1;
-			arr->len = 0;
-			return ERR_INVAL;
-		}
-		flag = '+';		                                                 //Reset flag to positive sign 			             
+	    } 
+	while(isSpace(pos[i])){
+	    i++;
+	}
+	if(pos[i] ==']'){
+	    (arr->uptr)[k] = num;
+	    arr->len = k+1;
+	    *p = i+1;
+	    return 0;
+	}
+	if(isInteger(pos[i])){
+	    *p = i+1;
+	    arr->len = 0;
+	    return ERR_INVAL;
+	}
+	if(pos[i] ==','){                                                			
+	    if(flag =='-'){
+	        num = -(num);
+	    }
+	    (arr->iptr)[k] = num; 
+	        k++;
+		i++;
+	}
+	if(pos[i] =='\0'){
+	    *p = i+1;
+	    arr->len = 0;
+	    return ERR_INVAL;
+	}
+	flag = '+';		                                                 //Reset flag to positive sign 			             
     }
 }
-
