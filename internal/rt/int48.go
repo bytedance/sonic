1afcc20807d2b258608fb20f6e2034f5d4ba6072
hongxuebo <hongxuebo@bytedance.com>
2023-09-04 18:10:32 +0800

    fix compile go x86 on windows

--- internal/rt/int48.go
+++ internal/rt/int48.go
@@ -17,20 +17,20 @@
 package rt
 
 const (
-    MinInt48 = -(1 << 47)
-    MaxInt48 = +(1 << 47) - 1
+	MinInt48 int64 = -(1 << 47)
+	MaxInt48 int64 = +(1 << 47) - 1
 )
 
 func PackInt(v int) uint64 {
-    if u := uint64(v); v < MinInt48 || v > MaxInt48 {
-        panic("int48 out of range")
-    } else {
-        return ((u >> 63) << 47) | (u & 0x00007fffffffffff)
-    }
+	if u := uint64(v); int64(v) < MinInt48 || int64(v) > MaxInt48 {
+		panic("int48 out of range")
+	} else {
+		return ((u >> 63) << 47) | (u & 0x00007fffffffffff)
+	}
 }
 
 func UnpackInt(v uint64) int {
-    v &= 0x0000ffffffffffff
-    v |= (v >> 47) * (0xffff << 48)
-    return int(v)
+	v &= 0x0000ffffffffffff
+	v |= (v >> 47) * (0xffff << 48)
+	return int(v)
 }
