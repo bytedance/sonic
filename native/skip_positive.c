8b11e547ccd74842c7a68de52d54500ff0a4e25c
谢更新 <xiegengxin.94@bytedance.com>
2023-09-25 14:03:25 +0800

    tmp

---
+++ native/skip_positive.c
@@ -0,0 +1,5 @@
+#include "scanning.h"
+
+long skip_positive(const GoString *src, long *p) {
+    return skip_positive_1(src, p);
+}
---
+++ native/vsigned.c
@@ -0,0 +1,6 @@
+#include "scanning.h"
+
+INLINE_ALL void vsigned(const GoString *src, long *p, JsonState *ret) {
+    int64_t sgn = 1;
+    vinteger(int64_t, sgn, sgn = -1)
+}
