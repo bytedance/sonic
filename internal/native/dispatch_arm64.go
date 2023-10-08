c5cd407848daf466fe33a818300b5eb99871f659
duanyi.aster <duanyi.aster@bytedance.com>
2023-10-08 16:23:19 +0800

    link arm native funcs to go stubs

--- internal/native/dispatch_arm64.go
+++ internal/native/dispatch_arm64.go
@@ -19,7 +19,7 @@ package native
 import (
 	`unsafe`
 
-	`github.com/bytedance/sonic/internal/native/neon`
+	neon `github.com/bytedance/sonic/internal/native/neon`
 	`github.com/bytedance/sonic/internal/native/types`
 )
 
@@ -60,67 +60,67 @@ var (
 
 //go:nosplit
 //go:noescape
-//goland:noinspection GoUnusedParameter
+//go:linkname Quote github.com/bytedance/sonic/internal/native/neon.__quote
 func Quote(s unsafe.Pointer, nb int, dp unsafe.Pointer, dn *int, flags uint64) int
 
 //go:nosplit
 //go:noescape
-//goland:noinspection GoUnusedParameter
+//go:linkname Unquote github.com/bytedance/sonic/internal/native/neon.__unquote
 func Unquote(s unsafe.Pointer, nb int, dp unsafe.Pointer, ep *int, flags uint64) int
 
 //go:nosplit
 //go:noescape
-//goland:noinspection GoUnusedParameter
+//go:linkname HTMLEscape github.com/bytedance/sonic/internal/native/neon.__html_escape
 func HTMLEscape(s unsafe.Pointer, nb int, dp unsafe.Pointer, dn *int) int
 
 //go:nosplit
 //go:noescape
-//goland:noinspection GoUnusedParameter
+//go:linkname Value github.com/bytedance/sonic/internal/native/neon.__value
 func Value(s unsafe.Pointer, n int, p int, v *types.JsonState, flags uint64) int
 
 //go:nosplit
 //go:noescape
-//goland:noinspection GoUnusedParameter
+//go:linkname SkipOne github.com/bytedance/sonic/internal/native/neon.__skip_one
 func SkipOne(s *string, p *int, m *types.StateMachine, flags uint64) int
 
 //go:nosplit
 //go:noescape
-//goland:noinspection GoUnusedParameter
+//go:linkname SkipOneFast github.com/bytedance/sonic/internal/native/neon.__skip_one_fast
 func SkipOneFast(s *string, p *int) int
 
 //go:nosplit
 //go:noescape
-//goland:noinspection GoUnusedParameter
+//go:linkname GetByPath github.com/bytedance/sonic/internal/native/neon.__get_by_path
 func GetByPath(s *string, p *int, path *[]interface{}, m *types.StateMachine) int
 
 //go:nosplit
 //go:noescape
-//goland:noinspection GoUnusedParameter
+//go:linkname ValidateOne github.com/bytedance/sonic/internal/native/neon.__validate_one
 func ValidateOne(s *string, p *int, m *types.StateMachine) int
 
 //go:nosplit
 //go:noescape
-//goland:noinspection GoUnusedParameter
+//go:linkname I64toa github.com/bytedance/sonic/internal/native/neon.__i64toa
 func I64toa(out *byte, val int64) (ret int)
 
 //go:nosplit
 //go:noescape
-//goland:noinspection GoUnusedParameter
+//go:linkname U64toa github.com/bytedance/sonic/internal/native/neon.__u64toa
 func U64toa(out *byte, val uint64) (ret int)
 
 //go:nosplit
 //go:noescape
-//goland:noinspection GoUnusedParameter
+//go:linkname F64toa github.com/bytedance/sonic/internal/native/neon.__f64toa
 func F64toa(out *byte, val float64) (ret int)
 
 //go:nosplit
 //go:noescape
-//goland:noinspection GoUnusedParameter
+//go:linkname ValidateUTF8 github.com/bytedance/sonic/internal/native/neon.__validate_utf8
 func ValidateUTF8(s *string, p *int, m *types.StateMachine) (ret int)
 
 //go:nosplit
 //go:noescape
-//goland:noinspection GoUnusedParameter
+//go:linkname ValidateUTF8Fast github.com/bytedance/sonic/internal/native/neon.__validate_utf8_fast
 func ValidateUTF8Fast(s *string) (ret int)
 
 func useNeon() {
