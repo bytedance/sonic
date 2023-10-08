a6adc9f3c2533b9f64fd27af615065558b95c3ac
duanyi.aster <duanyi.aster@bytedance.com>
2023-10-08 16:53:10 +0800

    test

--- ast/api_amd64_test.go
+++ ast/api_amd64_test.go
@@ -26,6 +26,7 @@ import (
     `runtime/debug`
     `testing`
 
+    `github.com/bytedance/sonic/encoder`
     `github.com/stretchr/testify/require`
     `github.com/bytedance/sonic/internal/native/types`
     `github.com/stretchr/testify/assert`
