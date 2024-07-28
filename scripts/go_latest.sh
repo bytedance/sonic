bf736e54fb6c54df3ff0d40f4045446ec6c607c1
duanyi.aster <duanyi.aster@bytedance.com>
2024-07-28 17:40:34 +0800

    update ci

--- scripts/go_latest.sh
+++ scripts/go_latest.sh
@@ -6,19 +6,6 @@ git clone https://github.com/golang/go.git $TAG
 cd $TAG/src
 git checkout -b build/$TAG $TAG 
 
-./all.bash
+./all.bash > /dev/null
 
-cd ..
-export GOROOT=$PWD
-export PATH=$GOROOT/bin:$PATH
-go version
 
-cd ../loader
-GOMAXPROCS=4 go test -v -race github.com/bytedance/sonic/loader
-
-cd ..
-GOMAXPROCS=4 go test -v -race .
-GOMAXPROCS=4 go test -v -race github.com/bytedance/sonic/ast
-GOMAXPROCS=4 go test -v -race github.com/bytedance/sonic/encoder
-GOMAXPROCS=4 go test -v -race github.com/bytedance/sonic/decoder
-GOMAXPROCS=4 go test -v -race github.com/bytedance/sonic/issue_test
