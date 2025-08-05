ef5542db0e71d0e5ff2c3a62e35427e1ffbf72ee
duanyi.aster <duanyi.aster@bytedance.com>
2025-08-05 21:54:52 +0800

    fix(encoder): not panic for unsupported map-key type

---
+++ issue_test/issue824_test.go
@@ -0,0 +1,32 @@
+package issue
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/bytedance/sonic"
+	"github.com/stretchr/testify/require"
+)
+
+/**
+ * Copyright 2025 ByteDance Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+func TestFloatMap(t *testing.T) {
+    js, err := sonic.ConfigStd.Marshal(map[float64]string{1: ""})
+    js0, err0 := json.Marshal(map[float64]string{1: ""})
+	require.Equal(t, err0 == nil, err == nil)
+	require.Equal(t, string(js0), string(js))
+}
