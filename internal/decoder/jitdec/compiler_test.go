e8dac213c8e8f7eca09760abcaf328a22daa3a8e
duanyi.aster <duanyi.aster@bytedance.com>
2026-03-26 21:31:48 +0800

    chore: update loader go mod

--- internal/decoder/jitdec/compiler_test.go
+++ internal/decoder/jitdec/compiler_test.go
@@ -21,7 +21,6 @@ import (
 	"testing"
 
 	"github.com/bytedance/sonic/internal/encoder/vars"
-	"github.com/bytedance/sonic/option"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -41,35 +40,5 @@ func BenchmarkCompiler_Compile(b *testing.B) {
 		as.name = "twitter struct"
 		_ = as.Load()
 		vars.ResetProgramCache()
-	}
-}
-
-
-type testPretouchManyA struct {
-	Name string             `json:"name"`
-	B    *testPretouchManyB `json:"b,omitempty"`
-}
-
-type testPretouchManyB struct {
-	ID int                `json:"id"`
-	A  *testPretouchManyA `json:"a,omitempty"`
-}
-
-func TestPretouchMany(t *testing.T) {
-	err := PretouchMany(
-		[]reflect.Type{reflect.TypeOf(testPretouchManyA{}), reflect.TypeOf(testPretouchManyB{})},
-		option.WithCompileMaxInlineDepth(1),
-		option.WithCompileRecursiveDepth(8),
-	)
-	assert.NoError(t, err)
-
-	s := `{"name":"x","b":{"id":7}}`
-	i := 0
-	var out testPretouchManyA
-	err = Decode(&s, &i, 0, &out)
-	assert.NoError(t, err)
-	assert.Equal(t, "x", out.Name)
-	if assert.NotNil(t, out.B) {
-		assert.Equal(t, 7, out.B.ID)
 	}
 }
